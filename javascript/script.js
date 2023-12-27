fcf0a170eee8ea9b0ce9284d3157c7611dc5677e
Renanluizssx <renanluizssx@gmail.com>
2023-12-27 02:43:11 -0300

    commitado

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("button");
 button.addEventListener("click", addLink);
 function addLink() {
-  let url = "paginas/AddPost.html";
+  let url = "./paginas/AddPost.html";
   window.open(url, "_self");
 }
